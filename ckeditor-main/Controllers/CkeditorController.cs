a46a372d7166eb76136c02ebb69fc06ea141328d
dineshkumar-stack <stardinesh4@gmail.com>
2023-11-14 18:57:41 +0530

    Editor added

---
+++ ckeditor-main/Controllers/CkeditorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ckeditor_main.Controllers
+{
+    public class CkeditorController : Controller
+    {
+        public IActionResult Editor()
+        {
+            return View();
+        }
+    }
+}
---
+++ ckeditor-main/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ckeditor_main.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
